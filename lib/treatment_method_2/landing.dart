69c63d875b8fad60c1fc3cea84391c32a19c1e57
Dileeshana <dileeshana2000@gmail.com>
2023-09-03 22:40:27 +0530

    doctor list done with error

--- lib/treatment_method_2/landing.dart
+++ lib/treatment_method_2/landing.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'doctor_list.dart';
+
 class HighSeverityLanding extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -26,7 +28,6 @@ class HighSeverityLanding extends StatelessWidget {
                           children: [
                             TextSpan(text: "Let's start your\n"),
                             TextSpan(text: "Consultation\n"),
-
                           ]
                         )
                         ),
@@ -53,28 +54,29 @@ class HighSeverityLanding extends StatelessWidget {
                 )
               ],
             ),),
-          Container(
-            width: 250,
-            height: 60,
-            alignment: Alignment.center,
-            margin: EdgeInsets.symmetric(vertical: 65, horizontal: 35),
-            decoration: BoxDecoration(
-              border: Border.all(color: Color.fromARGB(221, 122, 122, 122)),
-              borderRadius: BorderRadius.circular(5)
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [RichText(
-                  text: TextSpan(
-                    style: TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.w500),
-                    children: [
-                      TextSpan(text: "Chose a mental Inspector"),
-                    ]
-                  )
-                  ),
-              ],
-            )
-          ),
+            _choseMentalInstructor(context),
+          // Container(
+          //   width: 250,
+          //   height: 60,
+          //   alignment: Alignment.center,
+          //   margin: EdgeInsets.symmetric(vertical: 65, horizontal: 35),
+          //   decoration: BoxDecoration(
+          //     border: Border.all(color: Color.fromARGB(221, 122, 122, 122)),
+          //     borderRadius: BorderRadius.circular(5)
+          //   ),
+          //   child: Column(
+          //     mainAxisAlignment: MainAxisAlignment.center,
+          //     children: [RichText(
+          //         text: TextSpan(
+          //           style: TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.w500),
+          //           children: [
+          //             TextSpan(text: "Chose a mental Inspector"),
+          //           ]
+          //         )
+          //         ),
+          //     ],
+          //   )
+          // ),
           Container(
             margin: EdgeInsets.symmetric(vertical: 20),
             child: Center(
@@ -88,6 +90,28 @@ class HighSeverityLanding extends StatelessWidget {
             ),
             )
         ],
+      ),
+    );
+  }
+
+  _choseMentalInstructor(BuildContext context) {
+     return Container(
+      width: 250,
+      height: 60,
+      child: ElevatedButton(
+        child: Text(
+          "Chose a mental Inspector",
+          style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+        ),
+        style: ElevatedButton.styleFrom(
+            shape: LinearBorder(),
+            primary: Color.fromARGB(246, 209, 209, 209),
+            onPrimary: Colors.black
+            ),
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => DoctorList()));
+        },
       ),
     );
   }
